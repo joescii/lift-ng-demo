138e340cd063fc325b88c61a28414b953a1a2c66
Joe Barnes <barnesjd@gmail.com>
2015-07-19 15:22:19 -0500

    Grabbing lines out of source files

--- src/main/scala/net/liftmodules/ng/demo/snippet/Code.scala
+++ src/main/scala/net/liftmodules/ng/demo/snippet/Code.scala
@@ -3,19 +3,36 @@ package net.liftmodules.ng.demo.snippet
 import net.liftweb.http.{LiftRules, S}
 import net.liftweb.util.Helpers._
 
+import scala.util.Try
 import scala.xml.NodeSeq
 
 object Code {
+  private val newline = System.getProperty("line.separator")
+
   private def src = (for {
     name <- S.attr("src")
     code <- LiftRules.loadResourceAsString(name)
-  } yield { code })
-    .openOr("// Source not found!!!")
+  } yield {
+    firstLine.map { first =>
+      val last = lastLine.getOrElse(first)
+      val lines = code.split("(?s)\r?\n")
+      lines
+        .drop(first - 1)
+        .take(last - first + 1)
+        .mkString(newline)
+    }.getOrElse(code)
+  }).openOr("// Source not found!!!")
 
   private def lang = S.attr("src").map(_.split("\\.").last match {
     case "js" => "javascript"
     case other => other
   }).openOr("")
+
+  private def lineParse(which:Array[String] => String) = S.attr("l").toOption.flatMap( l =>
+    Try(which(l.split("-")).toInt).toOption
+  )
+  private def firstLine = lineParse(_.head)
+  private def lastLine  = lineParse(_.last)
 
   def render(in:NodeSeq):NodeSeq =
     <pre>
--- src/main/webapp/js/ServerTime.js
+++ src/main/webapp/js/ServerTime.js
@@ -1,9 +1,16 @@
-angular.module("ServerTimeApp", ["ServerTimeServices"])
-.controller("ServerTimeController", ["$scope", "ServerTimeService", function($scope, service){
-  $scope.fetch = function() {
-    service.currentTime().then(function(timestamp) {
-      $scope.latest = timestamp;
-    });
-  };
+angular.module("ServerTimeApp",
+  ["ServerTimeServices"]
+)
+
+.controller("ServerTimeController",
+  ["$scope", "ServerTimeService",
+    function($scope, service) {
+      $scope.fetch = function() {
+        service.currentTime().then(
+          function(timestamp) {
+            $scope.latest = timestamp;
+          }
+        );
+      };
 }])
 ;

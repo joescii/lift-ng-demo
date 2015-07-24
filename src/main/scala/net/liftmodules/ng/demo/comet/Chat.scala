9477e0333942a0596d726bb228b1921ce3414e6b
Joe Barnes <barnesjd@gmail.com>
2015-07-24 18:39:07 -0500

    Added code snips for chat

--- src/main/scala/net/liftmodules/ng/demo/comet/Chat.scala
+++ src/main/scala/net/liftmodules/ng/demo/comet/Chat.scala
@@ -4,17 +4,20 @@ import net.liftmodules.ng.AngularActor
 import net.liftweb.actor.LiftActor
 import net.liftweb.http.{CometListener, ListenerManager}
 
-object ChatServer extends LiftActor with ListenerManager {
-  override def createUpdate = ""
+object ChatServer extends LiftActor
+  with ListenerManager {
   override def lowPriority = {
     case msg:String =>
       sendListenersMessage(msg)
   }
+  override def createUpdate = ""
 }
 
-class ChatComet extends AngularActor with CometListener {
+class ChatComet extends AngularActor
+  with CometListener {
   override def registerWith = ChatServer
   override def lowPriority = {
-    case msg:String => scope.emit("new-message", msg)
+    case msg:String =>
+      scope.emit("new-message", msg)
   }
 }
--- src/main/webapp/js/chat.js
+++ src/main/webapp/js/chat.js
@@ -3,14 +3,16 @@ angular.module("ChatApp", ["ChatServices"])
   ["$scope", "ChatService",
     function($scope, service){
       $scope.messages = [];
+      $scope.$on("new-message",
+        function(e, msg){
+          $scope.messages.push(msg);
+        }
+      );
+
       $scope.sendChat = function() {
         service.send($scope.message);
         $scope.message = "";
       };
-      $scope.$on("new-message", function(e, msg){
-        $scope.messages.push(msg);
-      });
-
       $scope.onKeypress = function(e) {
         if(e.keyCode === 13) $scope.sendChat();
       }

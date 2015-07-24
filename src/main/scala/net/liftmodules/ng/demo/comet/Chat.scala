807e41c0a1c9f0a1c0e38c7f9750aded2a0865e6
Joe Barnes <barnesjd@gmail.com>
2015-07-24 18:19:34 -0500

    Trimmed the chat example code

--- src/main/scala/net/liftmodules/ng/demo/comet/Chat.scala
+++ src/main/scala/net/liftmodules/ng/demo/comet/Chat.scala
@@ -4,14 +4,10 @@ import net.liftmodules.ng.AngularActor
 import net.liftweb.actor.LiftActor
 import net.liftweb.http.{CometListener, ListenerManager}
 
-case class Messages(messages:List[String])
-
 object ChatServer extends LiftActor with ListenerManager {
-  private var messages = List.empty[String]
-  override def createUpdate = Messages(messages)
+  override def createUpdate = ""
   override def lowPriority = {
     case msg:String =>
-      messages = (messages :+ msg) takeRight 15
       sendListenersMessage(msg)
   }
 }
@@ -19,7 +15,6 @@ object ChatServer extends LiftActor with ListenerManager {
 class ChatComet extends AngularActor with CometListener {
   override def registerWith = ChatServer
   override def lowPriority = {
-    case Messages(msgs) => scope.assign("messages", msgs) // TODO: Doesn't seem to work??
-    case msg:String     => scope.emit("new-message", msg)
+    case msg:String => scope.emit("new-message", msg)
   }
 }

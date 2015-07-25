a2ea05b2755718ccbacf01cacbdcdb89fd26193c
Joe Barnes <barnesjd@gmail.com>
2015-07-25 11:28:23 -0500

    Added code slides for the editor

--- src/main/scala/net/liftmodules/ng/demo/comet/Editor.scala
+++ src/main/scala/net/liftmodules/ng/demo/comet/Editor.scala
@@ -6,19 +6,37 @@ import net.liftweb.common.Empty
 import net.liftweb.http.S
 import net.liftweb.util.MarkdownParser
 
-case class Input(input:String) extends NgModel
-case class Output(dom:String) extends NgModel
+import scala.xml.NodeSeq
 
-class InputBinder extends SimpleNgModelBinder("input", Input("")) with BindingToServer {
-  override val onClientUpdate = { input:Input =>
+case class Input(mdtext:String) extends NgModel
+case class Output(dom:String) extends NgModel
+object Output {
+  def apply(html:NodeSeq):Output = Output(html.toString)
+}
+
+class InputBinder
+  extends SimpleNgModelBinder(
+    "input",   // Bind to $scope.input
+    Input("")  // Initial value
+  ) with BindingToServer {
+  override val onClientUpdate =
+  { input:Input =>
     for {
       session <- S.session
-      content <- MarkdownParser.parse(input.input)
+      content <- MarkdownParser.parse(input.mdtext)
     } {
-      session.sendCometActorMessage("OutputBinder", Empty, Output(content.toString))
+      session.sendCometActorMessage(
+        "OutputBinder",
+        Empty, // Comet actors optionally have names
+        Output(content)
+      )
     }
     input
   }
 }
 
-class OutputBinder extends SimpleNgModelBinder("output", Output(<div>blah</div>.toString)) with BindingToClient with SessionScope
+class OutputBinder
+  extends SimpleNgModelBinder(
+    "output",  // Bind to $scope.output
+    Output(<div></div>)
+  ) with BindingToClient with SessionScope
